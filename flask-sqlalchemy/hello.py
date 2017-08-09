caeaa92b5a7ded11b0d86adf5500214a13eeaefe
oranbebas <oranbebas@gmail.com>
2017-08-09 14:06:44 +0900

    finish

---
+++ flask-sqlalchemy/hello.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hello.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80))
+
+    def __init__(self, username):
+        self.username = username
+
+@app.route("/")
+def hello():
+    user_list = User.query.all()
+    return render_template('hello.html', user_list=user_list)
+
+@app.route("/add_user", methods=['POST'])
+def add_user():
+    username = request.form.get('username')
+    if username:
+        user = User(username)
+        db.session.add(user)
+        db.session.commit()
+    return redirect(url_for('hello'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
